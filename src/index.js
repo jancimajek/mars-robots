9dba96d0df15613b248ee778697e8b0b1ebbfac3
Jan Majek <jan.majek@ft.com>
2018-10-07 22:53:33 +0100

    Add rough input parsing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import { createStore } from 'redux';
+import readline from 'readline';
+import fs from 'fs';
 import reducer from './reducer';
 import debug from './utils/debug';
 
 process.env.APP_NAME = process.env.APP_NAME || 'robots';
 const store = createStore(reducer);
 debug('*')(store);
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('input.txt', 'utf8'),
+  crlfDelay: Infinity,
+});
+
+rl.on('line', (line) => {
+  const [noComments] = line.split('#');
+  const l = noComments.trim();
+
+  // Ignore blank lines
+  if (l === '') return;
+
+  debug('input')('%o', l);
+});
+
+rl.on('close', () => {
+  debug('input')('<EOF>');
+});

fb6f9e2e9375272c496942ca73fff120cb0359ba
Jan Majek <jan.majek@ft.com>
2018-10-07 22:16:59 +0100

    Rename app to robots

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { createStore } from 'redux';
 import reducer from './reducer';
 import debug from './utils/debug';
 
-process.env.APP_NAME = process.env.APP_NAME || 'app';
+process.env.APP_NAME = process.env.APP_NAME || 'robots';
 const store = createStore(reducer);
 debug('*')(store);
